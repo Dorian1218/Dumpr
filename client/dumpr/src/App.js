2bc3eb305188b8cb37ab0ee5f6df9782b9efe739
Dorian1218 <dmicthell1663@gmail.com>
2022-08-08 10:40:45 -0400

    removed old code

--- client/dumpr/src/App.js
+++ client/dumpr/src/App.js
@@ -7,21 +7,9 @@ import Chat from './components/Chat/Chat';
 const socket = io.connect("http://localhost:3001")
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-
-  const joinRoom = () => {
-    if (username !== "" && room !== "") {
-      socket.emit("join_room", room)
-    }
-  }
   return (
     <div className="App">
-      <h3>Join A Chat</h3>
-      <input type="text" placeholder='Join' onChange={(e) => {setUsername(e.target.value)}}/>
-      <input type="text" placeholder='Room ID...' onChange={(e) => {setRoom(e.target.value)}}/>
-      <button onClick={joinRoom}>Join A Room</button>
-      <Chat socket={socket} username={username} room={room}/>
+      
     </div>
   );
 }
