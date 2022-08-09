a9cec66311eae4b7e39d6daa85303734a6b0cf33
Rishon Ilao <rishonilao@gmail.com>
2022-08-09 18:52:50 -0400

    changed rightbar a bit

--- client/dumpr/src/components/rightbar/Rightbar.jsx
+++ client/dumpr/src/components/rightbar/Rightbar.jsx
@@ -6,14 +6,8 @@ export default function Rightbar({ profile }) {
   const HomeRightbar = () => {
     return (
       <>
-        <div className="birthdayContainer">
-          <img className="birthdayImg" src="assets/gift.png" alt="" />
-          <span className="birthdayText">
-            <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
-          </span>
-        </div>
         <img className="rightbarAd" src="assets/ad.png" alt="" />
-        <h4 className="rightbarTitle">Online Friends</h4>
+        <h4 className="rightbarTitle">Sellers You Might Like:</h4>
         <ul className="rightbarFriendList">
           {Users.map((u) => (
             <Online key={u.id} user={u} />
